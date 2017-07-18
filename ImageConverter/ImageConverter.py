d1c995c2eec044e15d553921069f8b8b9b2a6825
Arun H Garimella <kilimanjaro2@localhost.localdomain>
2017-07-18 18:30:16 +0530

    Made Training set same resolution

--- ImageConverter/ImageConverter.py
+++ ImageConverter/ImageConverter.py
@@ -7,12 +7,13 @@ import matplotlib
 pic = image.open("00001.png")
 array = np.asarray(pic)
 small = np.zeros(shape=(32,32,3))
+final = np.zeros(shape=(64,64,3))
 
 x = 0
 y = 0
 
-for i in range(0,64,2):
-    for j in range (0,64,2):
+for i in xrange(0,64,2):
+    for j in xrange (0,64,2):
         small[x][y][0] = array[i][j][0]/4 + array[i+1][j][0]/4 + array[i][j+1][0]/4 + array[i+1][j+1][0]/4
         small[x][y][1] = array[i][j][1]/4 + array[i+1][j][1]/4 + array[i][j+1][1]/4 + array[i+1][j+1][1]/4
         small[x][y][2] = array[i][j][2]/4 + array[i+1][j][2]/4 + array[i][j+1][2]/4 + array[i+1][j+1][2]/4
@@ -21,4 +22,10 @@ for i in range(0,64,2):
             x = x + 1
             y = 0
 
-scipy.misc.imsave('outfile.jpg', small)
+for i in xrange(0,64,1):
+    for j in xrange(0,64,1):
+        final[i][j][0] = small[i/2][j/2][0]
+        final[i][j][1] = small[i/2][j/2][1]
+        final[i][j][2] = small[i/2][j/2][2]
+
+scipy.misc.imsave('outfile.png', final)
